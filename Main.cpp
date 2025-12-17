ded34f43f1634900d92bec365426bc035c142ff1
GabeZib <160553407+ZibLabs@users.noreply.github.com>
2025-12-17 11:28:06 -0500

    Added color.
    Started adding color system to menu.

--- Main.cpp
+++ Main.cpp
@@ -1,45 +1,75 @@
 #include <ncursesw/ncurses.h>
 
-void startup();
+void setup();
+void printHome();
 
 int main(void)
 {
-    startup();
-    return 0;
-}
-
-void startup()
-{
-    enum
-    {
-        ERROR = 1,
-        SUCCESS = 2,
-        WARNING = 3,
-        TITLE = 4
-    };
-    init_pair(ERROR, COLOR_WHITE, COLOR_RED);
-    init_pair(SUCCESS, COLOR_WHITE, COLOR_GREEN);
-    init_pair(9, COLOR_WHITE, COLOR_YELLOW);
-    init_pair(TITLE, COLOR_CYAN, COLOR_BLACK);
-    initscr();
-    start_color();
-    use_default_colors();
-
-    init_pair(1, COLOR_RED, COLOR_BLACK);
-    attron(COLOR_PAIR(1));
-
-    printw("COLORS = %d\n", COLORS);
-    printw("COLOR_PAIRS = %d\n", COLOR_PAIRS);
-    printw("can_change_color = %d\n", can_change_color());
-    attroff(COLOR_PAIR(1));
-    if (!(can_change_color())){
-        attron(COLOR_PAIR(9));
-        printw("Terminal does not support custom colors");
-    }
-
-    
-
+    setup();
+    printHome();
     refresh();
     getch();
     endwin();
+    return 0;
+}
+enum
+{
+
+    BLACK = 0,
+    RED = 1,
+    GREEN = 2,
+    YELLOW = 3,
+    BLUE = 4,
+    MAGENTA = 5,
+    CYAN = 6,
+    WHITE = 7,
+    ERROR = 10,
+    SUCCESS = 11,
+    WARNING = 12,
+    INFO = 13,
+    STANDARD = 14,
+
+};
+void setup()
+{
+    initscr();
+    start_color();
+    use_default_colors();
+    init_pair(ERROR, COLOR_MAGENTA, COLOR_RED);
+    init_pair(SUCCESS, COLOR_CYAN, COLOR_GREEN);
+    init_pair(WARNING, COLOR_RED, COLOR_YELLOW);
+    init_pair(INFO, COLOR_CYAN, COLOR_BLUE);
+    init_pair(STANDARD, COLOR_BLACK, COLOR_WHITE);
+    init_pair(BLACK, COLOR_BLACK, COLOR_BLACK);
+    init_pair(RED, COLOR_RED, COLOR_RED);
+    init_pair(GREEN, COLOR_GREEN, COLOR_GREEN);
+    init_pair(YELLOW, COLOR_YELLOW, COLOR_YELLOW);
+    init_pair(MAGENTA, COLOR_MAGENTA, COLOR_MAGENTA);
+    init_pair(CYAN, COLOR_CYAN, COLOR_CYAN);
+    init_pair(WHITE, COLOR_WHITE, COLOR_WHITE);
+}
+
+void printHome()
+{
+    for (int x = 0; x < COLS; x++)
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            init_pair(ERROR, COLOR_MAGENTA, COLOR_RED);
+            init_pair(SUCCESS, COLOR_CYAN, COLOR_GREEN);
+            init_pair(1, COLOR_RED, COLOR_YELLOW);
+            init_pair(INFO, COLOR_CYAN, COLOR_BLUE);
+            init_pair(STANDARD, COLOR_BLACK, COLOR_WHITE);
+            init_pair(BLACK, COLOR_BLACK, COLOR_BLACK);
+            init_pair(RED, COLOR_RED, COLOR_RED);
+            init_pair(GREEN, COLOR_GREEN, COLOR_GREEN);
+            init_pair(YELLOW, COLOR_YELLOW, COLOR_YELLOW);
+            init_pair(MAGENTA, COLOR_MAGENTA, COLOR_MAGENTA);
+            init_pair(CYAN, COLOR_CYAN, COLOR_CYAN);
+            init_pair(WHITE, COLOR_WHITE, COLOR_WHITE);
+            attron(1);
+            printw("g");
+            refresh();
+        }
+    }
 }
